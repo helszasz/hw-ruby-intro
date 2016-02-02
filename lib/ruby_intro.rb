40ef1a2ed8bd7f6b8f739dd2d4de89eaca027955
Hanna Elszasz <helszasz@wesleyan.edu>
2016-02-02 17:30:41 +0000

    finished part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,15 +3,37 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  if arr == []
+    return 0
+  else
+    sum_so_far = 0
+    arr.each {|n| sum_so_far += n}
+    return sum_so_far
+  end
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr == []
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  else
+    arr.sort!
+    while arr.length > 2  
+      arr.shift
+    end 
+    return sum(arr)
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr == []
+    return false
+  elsif arr.length == 1
+    return false
+  else 
+    return arr.permutation(2).any? {|x,y| x+y == n}
+  end
 end
 
 # Part 2
